1a89003b0070987a5049cd51a0d46fdc48837e46
samuelBabebe <samuel.abebe@miu.edu>
2021-11-03 18:27:12 -0500

    lab5 solution

--- lab5-asyncornous/lab5sol.js
+++ lab5-asyncornous/lab5sol.js
@@ -1,23 +1,41 @@
-function isPrime(num){
+// function isPrime(num){
+//     return new Promise((resolve,reject)=>{
+//        for(let i = 2, s = Math.sqrt(num); i <= s; i++){
+//                 if(num % i === 0){
+//                  reject({
+//                      prime:false
+//                  });
+//                 }
+//              }
+//             resolve({
+//                 prime:true
+//             })
+        
+        
+//     })
+// }
+// // isPrime(9).then((result)=>console.log(result))
+// //  .catch(result =>console.log(result))
+// console.log('start');
+// isPrime(7)
+//     .then(res => console.log(res))
+//     .catch(err => console.error(err));
+// console.log('end');
+
+Array.prototype.removeDuplicatesAsync =function(){
+    let oldarr = this;
     return new Promise((resolve,reject)=>{
-       for(let i = 2, s = Math.sqrt(num); i <= s; i++){
-                if(num % i === 0){
-                 reject({
-                     prime:false
-                 });
-                }
-             }
-            resolve({
-                prime:true
-            })
-        
-        
-    })
+        let arr =[]
+        for(item of oldarr){
+            if(arr.includes(item)){
+                
+            }else{
+                arr.push(item)
+            }
+        }
+       resolve(arr)
+    }).then(console.log)
 }
-// isPrime(9).then((result)=>console.log(result))
-//  .catch(result =>console.log(result))
-console.log('start');
-isPrime(7)
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
-console.log('end');
+console.log("lol");
+[4, 1, 5, 7, 2, 3, 1, 4, 6, 5, 2].removeDuplicatesAsync()
+console.log("kiya")
