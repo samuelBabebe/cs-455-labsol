937b4895083bd22c742c80c8cb72e6b69bd9e9cd
samuelBabebe <samuel.abebe@miu.edu>
2021-11-03 18:30:39 -0500

    final commit

--- lab5-asyncornous/lab5sol.js
+++ lab5-asyncornous/lab5sol.js
@@ -21,3 +21,21 @@ isPrime(7)
     .then(res => console.log(res))
     .catch(err => console.error(err));
 console.log('end');
+
+Array.prototype.removeDuplicatesAsync =function(){
+    let oldarr = this;
+    return new Promise((resolve,reject)=>{
+        let arr =[]
+        for(item of oldarr){
+            if(arr.includes(item)){
+                
+            }else{
+                arr.push(item)
+            }
+        }
+       resolve(arr)
+    }).then(console.log)
+}
+console.log("lol");
+[4, 1, 5, 7, 2, 3, 1, 4, 6, 5, 2].removeDuplicatesAsync()
+console.log("kiya")
